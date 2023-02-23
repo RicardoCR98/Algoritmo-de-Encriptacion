8107b99797e9bce3d0c5ef223774f477a727e845
RicardoCR98 <gary.campana@epn.edu.ec>
2023-02-23 16:55:16 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto2EDA_AlgoritmoEncriptacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
